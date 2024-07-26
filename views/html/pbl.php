48c37c81012b3e91b624506770f8c38d94f0ea59
Adam Wysocki <adam.wysocki@tpay.com>
2024-07-26 08:04:28 +0200

    Fix custom order function

--- views/html/pbl.php
+++ views/html/pbl.php
@@ -1,24 +1,28 @@
 <?php
+
 $renderType = tpayOption('global_render_payment_type');
 /** @var \Tpay\Dtos\Channel[] $list */
 $list = $this->channels();
-$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
-$tpay_gateways_list = \Tpay\TpayGateways::gateways_list();
+$availableGateways = WC()->payment_gateways()->get_available_payment_gateways();
 
-if ($available_gateways) {
-    foreach ($available_gateways as $available_gateway => $data) {
+if ($availableGateways) {
+    foreach ($availableGateways as $available_gateway => $data) {
         if (($available_gateway === TPAYGPAY_ID) && $data->enabled === 'yes') {
             $this->unset_banks[] = TPAYGPAY;
         }
+
         if (($available_gateway === TPAYBLIK_ID) && $data->enabled === 'yes') {
             $this->unset_banks[] = TPAYBLIK;
         }
+
         if ((in_array($available_gateway, [TPAYCC_ID, TPAYSF_ID])) && $data->enabled === 'yes') {
             $this->unset_banks[] = TPAYSF;
         }
+
         if (($available_gateway === TPAYTWISTO_ID) && $data->enabled === 'yes') {
             $this->unset_banks[] = TPAYTWISTO;
         }
+
         if (($available_gateway === TPAYINSTALLMENTS_ID) && $data->enabled === 'yes') {
             $this->unset_banks[] = TPAYINSTALLMENTS;
         }
@@ -30,18 +34,22 @@ if ($available_gateways) {
 }
 
 $list = $this->filter_out_constraints($list);
+$customOrderString = get_option('woocommerce_tpaypbl_settings')['custom_order'];
 
-if ($cr = get_option('woocommerce_tpaypbl_settings')['custom_order']) {
-    $new_list = [];
-    $cr = explode(',', $cr);
-    foreach ($list as $key => $item) {
-        if (in_array($item->id, $cr)) {
-            $new_list[] = $item;
-            unset($list[$key]);
+if ($customOrderString) {
+    $orderedList = [];
+    $customOrder = explode(',', $customOrderString);
+
+    foreach ($customOrder as $orderNumber) {
+        foreach ($list as $key => $item) {
+            if ($item->id == $orderNumber) {
+                $orderedList[] = $item;
+                unset($list[$key]);
+            }
         }
     }
 
-    $list = array_merge($new_list, $list);
+    $list = array_merge($orderedList, $list);
 }
 
 $list = array_filter($list, function (\Tpay\Dtos\Channel $channel) {
