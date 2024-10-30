b702275337bf768556f2ea2feb7f709180879be1
Karol Wojciechowski <karol.wojciechowski@tpay.com>
2024-10-30 21:56:21 +0100

    version

--- tpay.php
+++ tpay.php
@@ -3,7 +3,7 @@
  * Plugin Name: Tpay Payment Gateway
  * Plugin URI: https://tpay.com
  * Description: Tpay payment gateway for WooCommerce
- * Version: 1.7.6
+ * Version: 1.7.7
  * Author: Krajowy Integrator Płatności S.A.
  * Author URI: http://www.tpay.com
  * License: LGPL 3.0
