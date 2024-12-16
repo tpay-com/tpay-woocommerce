ce7221f1f91e4a29a526a61720264df1bec4f051
s4ddly <110701663+s4ddly@users.noreply.github.com>
2024-12-16 09:55:09 +0100

    Prevent card details inputs from being sent (#38)
    Co-authored-by: Adam Wysocki <adam.wysocki@tpay.com>

--- views/html/sf.php
+++ views/html/sf.php
@@ -5,25 +5,26 @@ if ($this->valid_mid) {
     $sf_rsa = get_option('woocommerce_tpaysf_settings')['sf_rsa'];
 }
 ?>
+<?php if ($sf_rsa): ?>
 <div class="tpay-sf" data-pubkey="<?php echo $sf_rsa ?>">
     <div class="tpay-sf-form">
         <img src="<?php echo esc_url(plugin_dir_url(__FILE__) . '../img/tpay-small.svg') ?>"/>
         <div class="card-container">
             <div class="card-number-container">
                 <label class="card-number"><?php esc_html_e('Card number', 'tpay'); ?>
-                    <input id="card_number" type="text" name="card-number" pattern="\d*" autocompletetype="cc-number" size="30"
+                    <input id="card_number" type="text" pattern="\d*" autocompletetype="cc-number" size="30"
                            type="tel" autocomplete="off" maxlength="23" placeholder="0000 0000 0000 0000" tabindex="1" value=""  style="" class="soft-wrong" />
                 </label>
             </div>
             <div class="date-container">
                 <label class="card-expiry"><?php esc_html_e('Expiry date', 'tpay') ?>
-                    <input id="expiry_date" type="text" name="card-expiry" placeholder="00 / 0000" autocomplete="off"
+                    <input id="expiry_date" type="text" placeholder="00 / 0000" autocomplete="off"
                            autocompletetype="cc-exp" tabindex="2" value="" style="" class="soft-wrong" />
                 </label>
             </div>
             <div class="cvc-container">
                 <label class="card-cvc"><?php esc_html_e('CVC', 'tpay') ?>
-                    <input id="cvc" type="text" name="card-cvc" placeholder="000" autocomplete="off" autocompletetype="cc-cvc"
+                    <input id="cvc" type="text" placeholder="000" autocomplete="off" autocompletetype="cc-cvc"
                            tabindex="3" value=""  style="" class="soft-wrong" />
                 </label>
             </div>
@@ -50,3 +51,4 @@ if ($this->valid_mid) {
     </div>
     <?php echo $agreements ?>
 </div>
+<?php endif ?>
