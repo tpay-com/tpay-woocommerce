692e03c6dd43bec462bfdbf4c71a17b53918cdec
kGablo <krzysztof.gablo@tpay.com>
2025-02-14 12:13:26 +0100

    Notification strategy fix

--- includes/Ipn/IpnContext.php
+++ includes/Ipn/IpnContext.php
@@ -2,14 +2,19 @@
 
 namespace Tpay\Ipn;
 
+use Tpay\Helpers\GatewayHelper;
+
 class IpnContext
 {
     public static function chooseStrategy($response)
     {
-        if ($response['tr_status']) {
+        if (isset($response['tr_status'])) {
             $strategy = new UpdateOrderStatus();
-        } elseif ($response['event']) {
+        } elseif (isset($response['event'])) {
             $strategy = new UpdateBlikAlias();
+        } else {
+            $text = implode("\n", array_map(fn($k, $v) => "$k:$v", array_keys($response), $response));
+            (new GatewayHelper())->tpay_logger('Mismatched strategy. Response content:  ' . $text);
         }
 
         return new ReceiveNotification($response, $strategy);
