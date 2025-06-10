3fca63c78c3ef2b97a30151fa9a88de8fd54463f
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-06-10 16:05:06 +0200

    Fixes issue with not applying channel contraints to generics in classic checkout

--- includes/TpayGateways.php
+++ includes/TpayGateways.php
@@ -113,7 +113,7 @@ abstract class TpayGateways extends WC_Payment_Gateway
             $id = $gatewayId;
         }
 
-        $values = $this->get_installments_values($id);
+        $values = $this->get_installments_values($id) + $this->get_generic_constraints();
 
         if (!isset($values[$id])) {
             return false;
@@ -645,6 +645,21 @@ abstract class TpayGateways extends WC_Payment_Gateway
             }
         }
 
+        return $values;
+    }
+
+    private function get_generic_constraints(): array
+    {
+        $genericPayments = tpayOption('global_generic_payments') ?? [];
+        $values = [];
+        foreach ($this->channels() as $channel) {
+            if (in_array($channel->id, $genericPayments) && isset($channel->constraints[1]->value)) {
+                $values['tpaygeneric-' . $channel->id] = [
+                    'min' => (float) $channel->constraints[0]->value,
+                    'max' => (float) $channel->constraints[1]->value,
+                ];
+            }
+        }
         return $values;
     }
 }
