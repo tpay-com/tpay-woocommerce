5def8f4574e28005c01c851e718b5aff3ff142bd
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-06-11 14:37:43 +0200

    Version bump (#58)
    Fixes version string in code

--- tpay.php
+++ tpay.php
@@ -3,7 +3,7 @@
  * Plugin Name: Tpay Payment Gateway
  * Plugin URI: https://tpay.com
  * Description: Tpay payment gateway for WooCommerce
- * Version: 1.8.4
+ * Version: 1.8.5
  * Author: Krajowy Integrator Płatności S.A.
  * Author URI: http://www.tpay.com
  * License: LGPL 3.0
