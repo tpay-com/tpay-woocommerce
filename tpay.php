360b2940a2a16dc1b6313c92cadad6fe60fa65cd
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-06-25 11:20:29 +0200

    Version 1.9.0 Changelog

--- tpay.php
+++ tpay.php
@@ -3,7 +3,7 @@
  * Plugin Name: Tpay Payment Gateway
  * Plugin URI: https://tpay.com
  * Description: Tpay payment gateway for WooCommerce
- * Version: 1.8.5
+ * Version: 1.9.0
  * Author: Krajowy Integrator Płatności S.A.
  * Author URI: http://www.tpay.com
  * License: LGPL 3.0
@@ -36,7 +36,7 @@ use Tpay\TpaySF;
 
 require_once 'tpay-functions.php';
 
-define('TPAY_PLUGIN_VERSION', '1.8.5');
+define('TPAY_PLUGIN_VERSION', '1.9.0');
 define('TPAY_PLUGIN_DIR', dirname(plugin_basename(__FILE__)));
 add_action('plugins_loaded', 'init_gateway_tpay');
 register_activation_hook(__FILE__, 'tpay_on_activate');
